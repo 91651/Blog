afa7854f677e61d587e127e8aead6d42fd3e5687
LZH <mail@521.org.cn>
2021-12-02 00:52:23 +0800

    markdown

--- App.Blazor.Admin/wwwroot/admin/js/app.js
+++ App.Blazor.Admin/wwwroot/admin/js/app.js
@@ -1,6 +1,22 @@
 ﻿// 动态加载JS
-window.appLoadJS = function (url) {
+window.appLoadJS = function (url, objRef, callback) {
     script = document.createElement("script");
     script.src = url;
+    script.addEventListener('load', e => {
+        objRef.invokeMethodAsync(callback);
+        console.log("aaaaaaaabbbbbb------------------1");
+    });
     document.body.appendChild(script);
+}
+
+window.appLoadJSWithWaiting = function (url) {
+    return new Promise((resolve, reject) => {
+        script = document.createElement("script");
+        script.src = url;
+        script.addEventListener('load', e => {
+            resolve();
+            console.log("aaaaaaaabbbbbb------------------2");
+        });
+        document.body.appendChild(script);
+    });
 }
