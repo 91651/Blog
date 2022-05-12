705e9324725d9be42f902b5e9bf07557ae5531dc
LZH <mail@521.org.cn>
2022-05-12 23:38:02 +0800

    fix: title render

--- App.Blazor.Client/Program.cs
+++ App.Blazor.Client/Program.cs
@@ -1,9 +1,13 @@
 using App.Blazor.Client.Data;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Refit;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
 var services = builder.Services;
 
 services.AddRefitClient<IDataProviderApi>().ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
