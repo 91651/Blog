5504b851c4056cd3af64320c24268b70ef3378b6
LZH <mail@521.org.cn>
2023-12-15 00:09:39 +0800

    channel

--- App.Business.Model/ChannelModel.cs
+++ App.Business.Model/ChannelModel.cs
@@ -1,11 +1,16 @@
-﻿namespace App.Business.Model
+﻿using System.ComponentModel;
+
+namespace App.Business.Model
 {
     public class ChannelModel
     {
         public string Id { get; set; }
         public string ParentId { get; set; }
+        [DisplayName("标题")]
         public string Title { get; set; }
+        [DisplayName("描述")]
         public string Description { get; set; }
+        [DisplayName("状态")]
         public int State { get; set; }
     }
 }
