f10f07ea4a0774e3135ed2cf37552e7b2b55573c
LZH <mail@521.org.cn>
2023-12-16 00:56:10 +0800

    article

---
+++ App.Business.Model/ChannelTreeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Business.Model
+{
+    public class ChannelTreeModel
+    {
+        public ChannelModel Channel { get; set; }
+        public ChannelTreeModel[] Children { get; set; }
+    }
+}
