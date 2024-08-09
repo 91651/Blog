dadfd090f8c443509002a9bbcd7f301c0c8e23d0
LZH <mail@521.org.cn>
2024-08-09 18:27:49 +0800

    dev

--- App.Blazor.Client/Program.cs
+++ App.Blazor.Client/Program.cs
@@ -8,6 +8,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var services = builder.Services;
 services.AddRefitClient<IDataProviderApi>().ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 services.AddHotKeys2();
-services.AddAntDesign();
 
 await builder.Build().RunAsync();
