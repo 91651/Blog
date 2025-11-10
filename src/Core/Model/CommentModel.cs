07331b164920e790be180c7390f6ca8720ae3a22
LZH <mail@521.org.cn>
2025-11-10 15:59:27 +0800

    fix: sumit comments

--- src/Core/Model/CommentModel.cs
+++ src/Core/Model/CommentModel.cs
@@ -9,7 +9,7 @@ public class CommentModel
     public string OwnerId { get; set; }
 
     [Required(ErrorMessage = "请输入一个心仪的昵称")]
-    [MinLength(3, ErrorMessage = "昵称至少3个字符")]
+    [MinLength(2, ErrorMessage = "昵称至少2个字符")]
     [MaxLength(20, ErrorMessage = "昵称最大长度20个字符")]
     public string Author { get; set; }
 
@@ -19,7 +19,7 @@ public class CommentModel
 
     [Required(ErrorMessage = "请输入想留下的话语")]
     [MinLength(5, ErrorMessage = "评论内容至少5个字符")]
-    [MaxLength(500, ErrorMessage = "评论内容最大长度500个字符")]
+    [MaxLength(1000, ErrorMessage = "评论内容最大长度1000个字符")]
     public string Content { get; set; }
 
     public DateTime Created { get; set; }
